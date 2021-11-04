b53fc339acc2af55e8004ac45cc70b94620b31f6
resyfer <resyfer.dev@gmail.com>
2021-11-04 12:46:44 +0530

    faq initial

--- public/js/index/index_faq.js
+++ public/js/index/index_faq.js
@@ -1,26 +1,31 @@
-var coll = document.getElementsByClassName("faq-btn");
-var i;
-for (i = 0; i < coll.length; i++) {
-coll[i].addEventListener("click", function() {
-var id = this.getAttribute('data-target');
-var content = document.getElementById(id);
-if (content.style.maxHeight){
- this.style.transform="rotate(0deg)";
-content.style.maxHeight = null;
-} else {
-  var collapsed = document.getElementsByClassName("faq-collapse");
-  var buttons = document.getElementsByClassName("faq-btn");
-  var j;
-  for(j=0;j<collapsed.length;j++)
-    {
-        if(collapsed[j].style.maxHeight)
-        {
-            buttons[j].style.transform="rotate(0deg)";
-            collapsed[j].style.maxHeight = null;
+let faqContainer = document.getElementsByClassName("faq-container")[0];
+let faqList = document.getElementsByClassName("faq");
+
+let states = [];
+for(let i = 0; i<faqList.length; i++)
+    states.push(0);
+
+for(let i = 0; i<faqList.length; i++) {
+    let question = faqList[i].getElementsByClassName("question")[0];
+
+    question.addEventListener("click", () => {
+        let answerList = faqContainer.getElementsByClassName("answer");
+        let buttonList = faqContainer.getElementsByClassName("button");
+
+        for(let j = 0; j<faqList.length; j++) {
+            if(j == i && states[j] == 0) {
+                answerList[j].style.height = "auto";
+                answerList[j].style.padding = "2vh";
+                answerList[j].style.opacity = "1";
+                buttonList[j].style.transform = "translate(0, -50%) rotate(180deg)";
+                states[j] = 1;
+            } else {
+                answerList[j].style.height = "0";
+                answerList[j].style.padding = "0";
+                answerList[j].style.opacity = "0";
+                buttonList[j].style.transform = "translate(0, -50%) rotate(0)";
+                states[j] = 0;
+            }
         }
-    }
-this.style.transform="rotate(180deg)";
-content.style.maxHeight = content.scrollHeight + "px";
-} 
-});
+    })
 }
