0b3b75549fb6c9e21192e17b8838706b50276e0b
resyfer <resyfer.dev@gmail.com>
2021-12-26 10:53:08 +0530

    switch cards

--- public/js/dscprojects/dscprojects.js
+++ public/js/dscprojects/dscprojects.js
@@ -57,3 +57,40 @@
 //   else
 //     cards[n].getElementsByClassName("go-back")[0].style.display = "block";
 // }
+
+const projects = document.getElementsByClassName("project-card");
+const projectCards = []; // 2D Array, each row is a card, each column is a section of card -> intro, info, members, tech-stack
+for (let i = 0; i<projects.length; i++) {
+  projectCards.push([]);
+  
+  let card = projects[i];
+  let subCards = card.getElementsByClassName("sub-card");
+
+  for(let j = 0; j<subCards.length; j++) {
+    projectCards[i][j] = subCards[j];
+  }
+}
+
+function toggle(index, subCard) {
+
+  let iconHover = projects[index].getElementsByClassName("icon-hover")[0];
+  for(let i = 0; i<projectCards[index].length; i++) {
+
+    if (i == subCard) {
+      projectCards[index][i].style.zIndex = +1;
+      projectCards[index][i].style.opacity = 1;
+    } else {
+      projectCards[index][i].style.zIndex = -1;
+      projectCards[index][i].style.opacity = 0;
+    }
+
+    if (subCard == 0) {
+      iconHover.style.display = "none";
+    } else {
+      iconHover.style.display = "block";
+      iconHover.style.left = `${25 * (subCard - 1)}%`;
+    }
+
+  }
+
+}
